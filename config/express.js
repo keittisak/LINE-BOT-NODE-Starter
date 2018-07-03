c039d67932954a8371441e9ee14c5b0291369bbd
Surachet Julakananusat <surachet.j@movefast.me>
2018-07-03 12:18:36 +0700

    update

--- config/express.js
+++ config/express.js
@@ -1,5 +1,7 @@
 module.exports = ()=>{
     const express = require('express');
+    const bodyParser = require('body-parser');
+    const line = require('../../../config/line');
     
     // create Express app
     // about Express itself: https://expressjs.com/
@@ -8,6 +10,12 @@ module.exports = ()=>{
     // Set up the routing.
     const v1 = express.Router();
     // const v2 = express.Router();
+
+    app.use('/lines/recieve', line.middleware)
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended:true
+    }))
 
     require('../app/routes/v1/index')(v1);
     require('../app/routes/v1/line')(v1);
