4f265ed0e187f9f94a0196fd8bb42f7ef029152d
Surachet Julakananusat <surachet.j@movefast.me>
2018-07-03 16:02:12 +0700

    update

--- app/controllers/v1/line.js
+++ app/controllers/v1/line.js
@@ -1,5 +1,6 @@
 const line = require('../../../config/line');
 const firebase = require('../../../config/firebase');
+const uuidv4 = require('uuid/v4');
 const db = firebase.firestore;
 
 exports.recieve = (req, res)=>{
@@ -35,14 +36,92 @@ function recieveMessageHandleEvent(data) {
     }
     
     //save data to firestore
+    var currentUser = 'DBvYKBUok0GKUob3vxjo';
+    var senderUser = '';
     var userRef = db.collection('users');
-
-    var data = {
-        channel: 'line',
-        id: data.source.userId
-    };
-    return userRef.add(data);
-
+    var conversationRef = db.collection('conversations');
+    return userRef
+        //check user exist
+        .where('id', '==', data.source.userId)
+        .where('channel', '==', 'line')
+        .get()
+        .then(snapshot => {
+            if (snapshot.size === 0) {
+                //create new user
+                var _data = {
+                    channel: 'line',
+                    id: data.source.userId
+                };
+                return userRef
+                    .add(_data)
+                    .then(ref => {
+                        //create new conversation
+                        conversationRef.add({
+                            created: Date.now(),
+                            users: [currentUser, ref.id],
+                            messages: []
+                    })
+                    .then(ref => { 
+                        return; 
+                    })
+                    .catch(err => {
+                        console.log('Error add documents', err);
+                    });
+                    return {"id": ref.id, "data": data};
+                })
+            }else{
+                var userIds = [];
+                var userDatas = [];
+                snapshot.forEach(doc => {
+                    userIds.push(doc.id);
+                    userDatas.push(doc.data());
+                });
+                return {"id": userIds[0], "data": userDatas[0]};
+            }
+        })
+        .then(user => {
+            //console.log(user);
+            senderUser = user.id;
+            //get message
+            conversationUsers = [currentUser, senderUser];
+            return conversationRef
+                .where('users', '==', conversationUsers)
+                .get()
+                .then(snapshot => {
+                    var conversationIds = [];
+                    var conversationDatas = [];
+                    snapshot.forEach(doc => {
+                        conversationIds.push(doc.id);
+                        conversationDatas.push(doc.data());
+                    });
+                    return {"id": conversationIds[0], "data": conversationDatas[0]};
+                })
+                .catch(err => {
+                    console.log('Error getting documents', err);
+                });
+        })
+        .then(conversation => {
+            //console.log(conversation);
+            var _data = {
+                created: Date.now(),
+                id: uuidv4(),
+                text: data.message.text, 
+                sender: senderUser
+            }
+            return conversationRef.doc(conversation.id).update({
+                messages: [...conversation.data.messages, _data]
+            })
+            .then((writeResult) => {
+                console.log("Process event success")
+                // return;
+            })
+            .catch(err => {
+                console.log('Error update documents', err);
+            });
+        })
+        .catch(err => {
+            console.log('Error getting documents', err);
+        });
 
     // // create a echoing text message
     // const echo = { type: 'text', text: data.message.text };
