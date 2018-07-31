a99c2966051942110ca1ef3460400a270f165bad
Surachet Julakananusat <surachet.j@movefast.me>
2018-07-31 14:26:44 +0700

    update

--- app/services/api.js
+++ app/services/api.js
@@ -3,8 +3,8 @@ var axios = require('axios');
 
 module.exports = function () {
     return axios.create({
-        // baseURL: 'https://admin.movefast.me/api',
-        baseURL: 'http://admin.movefast.loc/api',
+        baseURL: 'https://admin.movefast.me/api',
+        // baseURL: 'http://admin.movefast.loc/api',
         // timeout: 1000,
         headers: {
             'Accept': 'application/json',
